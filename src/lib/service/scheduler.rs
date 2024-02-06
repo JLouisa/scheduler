c55d0e774328366f7028defe2430dbb844cea574
JLouisa <jonathan.louisa@gmail.com>
2024-02-06 02:01:41 +0100

    Fix: Update some comments

--- src/lib/service/scheduler.rs
+++ src/lib/service/scheduler.rs
@@ -5,11 +5,14 @@ use crate::domain::availability::field;
 use crate::domain::availability::{self, Availability};
 use crate::domain::user::User;
 use crate::domain::{week, Role, ScheduleDay, ScheduleTime};
+
 use crate::service::Logic;
 
 use std::str::FromStr;
 
-#[derive(Debug)]
+use enum_iterator::all;
+
+#[derive(Debug, Clone)]
 pub struct Employee {
     id: availability::field::AvailabilityId,
     name: availability::field::Name,
@@ -18,7 +21,7 @@ pub struct Employee {
     chosen: u8,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Employees {
     manager: Vec<Employee>,
     griller: Vec<Employee>,
@@ -28,7 +31,7 @@ pub struct Employees {
     servers: Vec<Employee>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct DaySchedule {
     day: week::field::Days,
     employees: Employees,
@@ -53,18 +56,10 @@ pub fn get_user(
     role: &Role,
     day: &ScheduleDay,
 ) -> Vec<availability::Availability> {
+    //* Sorting lists
     // 1. Get list of all users with Management Role from users in DB
-    let binding1 = user_list.clone();
-    // let list_all_management_users = binding1
-    //     .iter()
-    //     .cloned()
-    //     .filter(|user| {
-    //         user.role_secondary.into_inner() == role || user.role_primary.into_inner() == role
-    //     })
-    //     .collect::<Vec<User>>();
-
-    // 2. Get list of Managers with vast = true from all users with Management Role
-    let list_vast_managers = binding1
+    let list_vast_managers = user_list
+        .clone()
         .iter()
         .cloned()
         .filter(|user| {
@@ -74,19 +69,19 @@ pub fn get_user(
         })
         .collect::<Vec<User>>();
 
-    // 1. Get list of all available users on Monday from available users
-    let monday_available_users = list
+    // 2. Get list of all available users on a given day from available users
+    let day_available_users = list
         .iter()
         .cloned()
         .filter(|availability| availability.day.into_inner() == day)
         .collect::<Vec<availability::Availability>>();
 
-    // 2. Get list of available managers on Monday from available users on Monday
+    // 3. Get list of available roles on a given day from available users on that day
     let monday_available_managers = {
         let mut new_vec = vec![];
 
         for manager in list_vast_managers.clone() {
-            for user in monday_available_users.clone() {
+            for user in day_available_users.clone() {
                 if user.user_id.to_the_string() == manager.id.to_the_string() {
                     new_vec.push((user).clone())
                 }
@@ -95,10 +90,6 @@ pub fn get_user(
         new_vec
     };
 
-    // for manager in monday_available_managers.iter() {
-    //     println!("User from {:?}: {:?}", role, manager);
-    // }
-
     // 1. List of managers
     // let list_managers = list_all_management_users;
     // 2. List of managers with vast = true && chosen < max day
@@ -106,7 +97,8 @@ pub fn get_user(
     // 3. Available managers
     let list_all_available_users = monday_available_managers;
 
-    //? <-------------------->
+    //? <--------------------
+    //* Logic to distribute the available users
 
     // Process:
     // check if list of available managers is empty
@@ -114,7 +106,7 @@ pub fn get_user(
         // println!("No available users on {:?} for {:?}", day, role);
         let generate_needed_manager = lib::get_user_with_highest_max_days(&list_vast_users, role);
 
-        // Check if the day is Monday to adjust the time
+        // Check if the day is Monday to adjust the start time
         let the_time = if day == &ScheduleDay::Monday {
             &ScheduleTime::StartAtOne
         } else {
@@ -164,7 +156,7 @@ pub fn get_user(
     }
 }
 
-//? <-------------------->
+//? -------------------->
 
 ///* Calculate the schedule for a role of the user
 pub fn calc_schedule_role(
@@ -217,51 +209,34 @@ pub fn calc_schedule_day(
     schedule_logic: &Logic,
     day: &ScheduleDay,
 ) -> DaySchedule {
-    let the_day = DaySchedule {
-        day: week::field::Days::create(day.to_owned()),
-        employees: Employees {
-            manager: calc_schedule_role(
+    // Create the vector to hold the roles
+    let mut roles: Vec<Vec<Employee>> = Vec::new();
+
+    // Using the enum iterator to loop through the roles
+    for role in all::<Role>() {
+        if role != Role::All || role != Role::None {
+            let position = calc_schedule_role(
                 all_availability,
                 all_users,
                 &schedule_logic.manager,
-                &Role::Management,
+                &role,
                 day,
-            ),
-            griller: calc_schedule_role(
-                all_availability,
-                all_users,
-                &schedule_logic.griller,
-                &Role::Griller,
-                day,
-            ),
-            kitchen: calc_schedule_role(
-                all_availability,
-                all_users,
-                &schedule_logic.kitchen,
-                &Role::Kitchen,
-                day,
-            ),
-            bar: calc_schedule_role(
-                all_availability,
-                all_users,
-                &schedule_logic.bar,
-                &Role::Bar,
-                day,
-            ),
-            dishwashers: calc_schedule_role(
-                all_availability,
-                all_users,
-                &schedule_logic.dishwashers,
-                &Role::Dishwasher,
-                day,
-            ),
-            servers: calc_schedule_role(
-                all_availability,
-                all_users,
-                &schedule_logic.servers,
-                &Role::Service,
-                day,
-            ),
+            );
+            roles.push(position)
+        }
+    }
+
+    // Create the day schedule with the roles
+    let the_day = DaySchedule {
+        day: week::field::Days::create(day.to_owned()),
+        employees: Employees {
+            // Add the role from roles to the employees
+            servers: roles.pop().expect("could not pop server roles"),
+            dishwashers: roles.pop().expect("could not pop dishwasher roles"),
+            bar: roles.pop().expect("could not pop bar roles"),
+            kitchen: roles.pop().expect("could not pop kitchen roles"),
+            griller: roles.pop().expect("could not pop griller roles"),
+            manager: roles.pop().expect("could not pop manager roles"),
         },
     };
     return the_day;
@@ -273,50 +248,22 @@ pub fn calc_schedule_week(
     all_users: &Vec<User>,
     schedule_logic: &Logic,
 ) -> WeekSchedule {
+    let mut week: Vec<DaySchedule> = Vec::new();
+
+    for day in all::<ScheduleDay>() {
+        let the_day = calc_schedule_day(&all_availability, &all_users, &schedule_logic, &day);
+        week.push(the_day)
+    }
+
     let week = WeekSchedule {
         weekly_id: week::field::WeeklyId::new(),
-        monday: calc_schedule_day(
-            &all_availability,
-            &all_users,
-            &schedule_logic,
-            &ScheduleDay::Monday,
-        ),
-        tuesday: calc_schedule_day(
-            &all_availability,
-            &all_users,
-            &schedule_logic,
-            &ScheduleDay::Tuesday,
-        ),
-        wednesday: calc_schedule_day(
-            &all_availability,
-            &all_users,
-            &schedule_logic,
-            &ScheduleDay::Wednesday,
-        ),
-        thursday: calc_schedule_day(
-            &all_availability,
-            &all_users,
-            &schedule_logic,
-            &ScheduleDay::Thursday,
-        ),
-        friday: calc_schedule_day(
-            &all_availability,
-            &all_users,
-            &schedule_logic,
-            &ScheduleDay::Friday,
-        ),
-        saturday: calc_schedule_day(
-            &all_availability,
-            &all_users,
-            &schedule_logic,
-            &ScheduleDay::Saturday,
-        ),
-        sunday: calc_schedule_day(
-            &all_availability,
-            &all_users,
-            &schedule_logic,
-            &ScheduleDay::Sunday,
-        ),
+        sunday: week.pop().expect("could not pop sunday"),
+        saturday: week.pop().expect("could not pop saturday"),
+        friday: week.pop().expect("could not pop friday"),
+        thursday: week.pop().expect("could not pop thursday"),
+        wednesday: week.pop().expect("could not pop wednesday"),
+        tuesday: week.pop().expect("could not pop tuesday"),
+        monday: week.pop().expect("could not pop monday"),
     };
     return week;
 }
