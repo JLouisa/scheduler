80862b10d81ee9d219e3c76853c44a1cd3adfb24
JLouisa <jonathan.louisa@gmail.com>
2024-02-07 16:56:32 +0100

    Refactoring Get user function

--- src/lib/service/lib.rs
+++ src/lib/service/lib.rs
@@ -1,10 +1,13 @@
+use crate::data::DbId;
 use crate::domain::availability;
+use crate::domain::availability::field;
 use crate::domain::availability::Availability;
 use crate::domain::user::User;
-use crate::domain::{Role, ScheduleDay};
+use crate::domain::{Role, ScheduleDay, ScheduleTime};
 
 use rand::Rng;
 use std::collections::HashMap;
+use std::str::FromStr;
 
 // Function to get a random number
 pub fn get_random_number(num: usize) -> usize {
@@ -15,7 +18,7 @@ pub fn get_random_number(num: usize) -> usize {
 }
 
 // Function that iterates through a list of users and returns the user with the highest max_days
-pub fn get_user_with_highest_max_days(list: &Vec<User>, role: &Role) -> Option<User> {
+pub fn get_user_with_highest_max_days(list: &Vec<User>) -> Option<User> {
     let mut num = 0;
     let mut the_user: Option<User> = None;
     for user in list.iter() {
@@ -24,7 +27,16 @@ pub fn get_user_with_highest_max_days(list: &Vec<User>, role: &Role) -> Option<U
             the_user = Some(user.clone());
         }
     }
-    return the_user;
+    match the_user {
+        Some(e) => return Some(e.clone()),
+        None => {
+            if !list.is_empty() {
+                return Some(list[0].clone());
+            } else {
+                None
+            }
+        }
+    }
 }
 
 // Function to sort the availability list
@@ -180,4 +192,117 @@ pub fn sort_available_users_on_role(
         }
     }
     new_vec
+}
+
+pub fn get_available_users(
+    list: &Vec<availability::Availability>,
+    user_list: &Vec<User>,
+    logic: &Vec<ScheduleTime>,
+    role: &Role,
+    day: &ScheduleDay,
+    chosen_users: &mut HashMap<String, u8>,
+) -> Vec<Option<availability::Availability>> {
+    // Debug
+    println!("In section 1");
+
+    //* Sorting lists
+    // 1. Get list of all vast users with current Role
+    let list_vast_users = sort_vast_users(user_list, role);
+
+    // 2. Get list of all available users on a given day
+    let current_day_available_users = sort_available_users(list, day);
+
+    // 3. Get list of available roles on a given day from available users on that day
+    let list_all_available_users =
+        sort_available_users_on_role(&list_vast_users, &current_day_available_users);
+
+    // 4. Get
+
+    //? <--------------------
+
+    //* Logic to distribute the available users
+    // Process:
+    //? check if list of available users is empty
+    if list_all_available_users.is_empty() {
+        let generate_needed_user = get_user_with_highest_max_days(&list_vast_users);
+
+        match generate_needed_user {
+            Some(g_user) => {
+                // Check if the day is Monday to adjust the start time
+                let the_time = if day == &ScheduleDay::Monday {
+                    &ScheduleTime::StartAtOne
+                } else {
+                    &ScheduleTime::StartAtThree
+                };
+
+                // Create new availability for the needed manager
+                let new_available_manager = availability::Availability {
+                    user_id: field::AvailabilityId::new(
+                        DbId::from_str(g_user.id.to_the_string().as_str())
+                            .expect("could not create id"),
+                    ),
+                    weekly_id: field::WeeklyId::new(),
+                    name: field::Name::new(g_user.name.as_str()).expect("could not create name"),
+                    day: field::Days::create(day.to_owned()),
+                    time: field::Time::create(the_time.to_owned()),
+                };
+                // Increase the chosen count for the user
+                increase_chosen_user_count(g_user.id.to_the_string().as_str(), chosen_users);
+
+                return vec![Some(new_available_manager)];
+            }
+            None => return vec![None],
+        }
+
+        //? check if the list of vast managers is empty
+    } else if list_vast_users.is_empty() {
+        // Debug
+        println!("In section 2");
+
+        // Assuming the type of elements in list_all_available_users is User
+        let mut the_user_role_list: Vec<Option<Availability>> = Vec::new();
+
+        // Sort list_all_available_users in-place by time
+        let list_all_available_users: Vec<Availability> =
+            bubble_sort(&mut list_all_available_users.to_owned());
+
+        // Sort the list of available users based on chosen count
+        let list_all_available_users: Vec<Option<Availability>> =
+            sort_lowest_to_highest_count(list_all_available_users, chosen_users, list_vast_users);
+
+        // Process the available list (x logic per role) amount of time
+        for i in 0..logic.len().to_owned() {
+            match &list_all_available_users[i] {
+                Some(user) => {
+                    the_user_role_list.push(Some(user.clone()));
+                }
+                None => the_user_role_list.push(None),
+            }
+        }
+        return the_user_role_list;
+
+        //? If the list of available Vast users is not empty
+    } else {
+        // Assuming the type of elements in list_all_available_users is User
+        let mut the_user_role_list: Vec<Option<Availability>> = Vec::new();
+
+        // Sort list_all_available_users by time
+        let list_all_available_users: Vec<Availability> =
+            bubble_sort(&mut list_all_available_users.to_owned());
+
+        // Sort the list of available users based on chosen count
+        let list_all_available_users =
+            sort_lowest_to_highest_count(list_all_available_users, chosen_users, list_vast_users);
+
+        // Process the available list (x logic per role) amount of time
+        for i in 0..logic.len().to_owned() {
+            match &list_all_available_users[i] {
+                Some(user) => {
+                    the_user_role_list.push(Some(user.clone()));
+                }
+                None => the_user_role_list.push(None),
+            }
+        }
+        return the_user_role_list;
+    }
 }
--- src/lib/service/scheduler.rs
+++ src/lib/service/scheduler.rs
@@ -1,14 +1,11 @@
 use super::lib;
 
-use crate::data::DbId;
-use crate::domain::availability::field;
 use crate::domain::availability::{self, Availability};
 use crate::domain::user::User;
 use crate::domain::{week, Role, ScheduleDay, ScheduleTime};
 use crate::service::Logic;
 
 use std::collections::HashMap;
-use std::str::FromStr;
 
 use enum_iterator::all;
 
@@ -58,123 +55,6 @@ pub struct WeekSchedule {
     sunday: DaySchedule,
 }
 
-pub fn get_users(
-    list: &Vec<availability::Availability>,
-    user_list: &Vec<User>,
-    logic: &Vec<ScheduleTime>,
-    role: &Role,
-    day: &ScheduleDay,
-    chosen_users: &mut HashMap<String, u8>,
-) -> Vec<Option<availability::Availability>> {
-    // Debug
-    println!("In section 1");
-
-    //* Sorting lists
-    // 1. Get list of all users with Management Role from users in DB
-    let list_vast_users = lib::sort_vast_users(user_list, role);
-
-    // 2. Get list of all available users on a given day from available users
-    let day_available_users = lib::sort_available_users(list, day);
-
-    // 3. Get list of available roles on a given day from available users on that day
-    let list_all_available_users =
-        lib::sort_available_users_on_role(&list_vast_users, &day_available_users);
-
-    //? <--------------------
-
-    //* Logic to distribute the available users
-    // Process:
-    //? check if list of available users is empty
-    if list_all_available_users.is_empty() {
-        let generate_needed_user = lib::get_user_with_highest_max_days(&list_vast_users, role);
-
-        match generate_needed_user {
-            Some(g_user) => {
-                // Increase the chosen count for the user
-                lib::increase_chosen_user_count(g_user.id.to_the_string().as_str(), chosen_users);
-
-                // Check if the day is Monday to adjust the start time
-                let the_time = if day == &ScheduleDay::Monday {
-                    &ScheduleTime::StartAtOne
-                } else {
-                    &ScheduleTime::StartAtThree
-                };
-
-                // Create new availability for the needed manager
-                let new_available_manager = availability::Availability {
-                    user_id: field::AvailabilityId::new(
-                        DbId::from_str(g_user.id.to_the_string().as_str())
-                            .expect("could not create id"),
-                    ),
-                    weekly_id: field::WeeklyId::new(),
-                    name: field::Name::new(g_user.name.as_str()).expect("could not create name"),
-                    day: field::Days::create(day.to_owned()),
-                    time: field::Time::create(the_time.to_owned()),
-                };
-                return vec![Some(new_available_manager)];
-            }
-            None => return vec![None],
-        }
-
-        //? check if the list of vast managers is empty
-    } else if list_vast_users.is_empty() {
-        // Debug
-        println!("In section 2");
-
-        // Assuming the type of elements in list_all_available_users is User
-        let mut the_user_role_list: Vec<Option<Availability>> = Vec::new();
-
-        // Sort list_all_available_users in-place by time
-        let list_all_available_users: Vec<Availability> =
-            lib::bubble_sort(&mut list_all_available_users.to_owned());
-
-        // Sort the list of available users based on chosen count
-        let list_all_available_users: Vec<Option<Availability>> = lib::sort_lowest_to_highest_count(
-            list_all_available_users,
-            chosen_users,
-            list_vast_users,
-        );
-
-        // Process the available list (x logic per role) amount of time
-        for i in 0..logic.len().to_owned() {
-            match &list_all_available_users[i] {
-                Some(user) => {
-                    the_user_role_list.push(Some(user.clone()));
-                }
-                None => the_user_role_list.push(None),
-            }
-        }
-        return the_user_role_list;
-
-        //? If the list of available Vast users is not empty
-    } else {
-        // Assuming the type of elements in list_all_available_users is User
-        let mut the_user_role_list: Vec<Option<Availability>> = Vec::new();
-
-        // Sort list_all_available_users by time
-        let list_all_available_users: Vec<Availability> =
-            lib::bubble_sort(&mut list_all_available_users.to_owned());
-
-        // Sort the list of available users based on chosen count
-        let list_all_available_users = lib::sort_lowest_to_highest_count(
-            list_all_available_users,
-            chosen_users,
-            list_vast_users,
-        );
-
-        // Process the available list (x logic per role) amount of time
-        for i in 0..logic.len().to_owned() {
-            match &list_all_available_users[i] {
-                Some(user) => {
-                    the_user_role_list.push(Some(user.clone()));
-                }
-                None => the_user_role_list.push(None),
-            }
-        }
-        return the_user_role_list;
-    }
-}
-
 //? -------------------->
 
 ///* Calculate the schedule for a role of the user
@@ -186,20 +66,11 @@ pub fn calc_schedule_role(
     day: &ScheduleDay,
     chosen_users: &mut HashMap<String, u8>,
 ) -> Vec<Employee> {
-    // println!("All users");
-    // println!("{:?}", user_list);
-
-    // println!("All availabilities");
-    // println!("{:?}", available_list);
-
-    // Process Manager
+    // Process Role
     let users: Vec<Option<Availability>> =
-        get_users(available_list, user_list, logic, role, day, chosen_users);
+        lib::get_available_users(available_list, user_list, logic, role, day, chosen_users);
 
-    println!("After get users:");
-    println!("{:?}", users);
-
-    let mut list = Vec::new();
+    let mut list_of_employees = Vec::new();
 
     // Debug
     println!("In section 3");
@@ -243,13 +114,13 @@ pub fn calc_schedule_role(
                     u_users.user_id.to_the_string().as_str(),
                     chosen_users,
                 );
-                list.push(new_employee);
+                list_of_employees.push(new_employee);
             }
-            None => list.push(Employee::no_user()),
+            None => list_of_employees.push(Employee::no_user()),
         }
     }
 
-    return list;
+    return list_of_employees;
 }
 
 ///* Calculate the schedule for a day of the week
